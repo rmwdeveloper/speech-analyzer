a4eaed46fb67bcf0666e2354ceb78c143879af8c
rmwdeveloper <rmwdeveloper@gmail.com>
2017-03-12 15:28:25 -0500

    csrf

--- speech/speech/views.py
+++ speech/speech/views.py
@@ -1,8 +1,12 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 
+
+@csrf_exempt
 def index(request):
     return render(request, 'index.html')
 
+@csrf_exempt
 def upload(request):
     return JsonResponse({'foo': 'bar'})
