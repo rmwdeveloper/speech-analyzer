0d139d0b4557a188f2b341c0e20b5b705b529372
rmwdeveloper <rmwdeveloper@gmail.com>
2017-03-12 14:32:55 -0500

    uploader with fallback

--- speech/react/components/Uploader/Uploader.js
+++ speech/react/components/Uploader/Uploader.js
@@ -4,20 +4,39 @@ export default function uploader(WrappedComponent, uploadUrl) {
   return class extends Component {
     constructor(){
       super();
+      this.test = this.test.bind(this);
+      this.clickFile = this.clickFile.bind(this);
+
+    }
+    test() {
+      this.input.click();
+    }
+    clickFile(event) {
+      for( let iterator = 0; iterator < event.target.files.length; iterator++){
+        this.dropzone.emit('addedfile', event.target.files[iterator]);
+      }
     }
     componentDidMount() {
+
       this.dropzone = new Dropzone(this.uploader, {url: '/',
         autoProcessQueue:false,
+        clickable: true,
         createImageThumbnails: false,
         previewsContainer: null,
         addedfile: file => {
           console.log(file);
         }
       });
+
+
+      this.uploader.addEventListener('click', this.test);
+      this.input.addEventListener('change', this.clickFile);
+
     }
     render() {
       return (<div style={{display: 'inline-block'}} ref={(uploader) => { this.uploader = uploader; }}>
-        <WrappedComponent {...this.props} />
+        <input ref={(input) => { this.input = input; }} style={{display: 'none'}} type="file" />
+        <WrappedComponent  {...this.props} />
       </div>);
     }
   }
