4a8be13a323806f1b9a4bc80643d9d8629239798
rmwdeveloper <rmwdeveloper@gmail.com>
2017-03-13 16:30:20 -0500

    record timestamps

--- speech/speech/helpers/speechToText.py
+++ speech/speech/helpers/speechToText.py
@@ -1,7 +1,11 @@
 import os
+import json
 from channels import Group
 from django.conf import settings
-import speech_recognition as sr
+from speech.models import Timestamp
+from watson_developer_cloud import SpeechToTextV1
+# import speech_recognition as sr
+
 
 
 def callAPI(instance):
@@ -9,18 +13,40 @@ def callAPI(instance):
     IBM_USERNAME = settings.IBM_USERNAME  # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
     IBM_PASSWORD = settings.IBM_PASSWORD  # IBM Speech to Text passwords are mixed-case alphanumeric strings
     AUDIO_FILE = instance.audio.file.name
-    r = sr.Recognizer()
-    with sr.AudioFile(AUDIO_FILE) as source:
-        audio = r.record(source)  # read the entire audio file
+    speech_to_text = SpeechToTextV1(
+        username=IBM_USERNAME,
+        password=IBM_PASSWORD,
+        x_watson_learning_opt_out=False
+    )
+    with open(AUDIO_FILE, 'rb') as audio_file:
 
-    try:
-        text = r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD)
-        instance.transcribedSpeech = text
+        response = speech_to_text.recognize(
+            audio_file, content_type='audio/wav', timestamps=True,
+            word_confidence=True)
+
+        data = response['results'][0]['alternatives'][0]
+        t0 = 'test'
+        instance.transcribedSpeech = data['transcript']
+        instance.transcriptionConfidence = data['confidence']
         instance.transcribed = True
         instance.save()
-        Group('main').send({'text': text})
-    except sr.UnknownValueError:
-        print("IBM Speech to Text could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from IBM Speech to Text service; {0}".format(e))
+
+        for timestamp in data['timestamps']:
+            Timestamp.objects.create(audio=instance, word = timestamp[0],
+                                     timestampBegin = timestamp[1], timestampEnd=timestamp[2])
+        # Group('main').send({'text': text})
+    # r = sr.Recognizer()
+    # with sr.AudioFile(AUDIO_FILE) as source:
+    #     audio = r.record(source)  # read the entire audio file
+    #
+    # try:
+    #     text = r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD)
+    #     instance.transcribedSpeech = text
+    #     instance.transcribed = True
+    #     instance.save()
+    #     Group('main').send({'text': text})
+    # except sr.UnknownValueError:
+    #     print("IBM Speech to Text could not understand audio")
+    # except sr.RequestError as e:
+    #     print("Could not request results from IBM Speech to Text service; {0}".format(e))
 
--- speech/speech/models.py
+++ speech/speech/models.py
@@ -8,7 +8,12 @@ class Audio(models.Model):
     transcoded = models.BooleanField(default=False)
     transcribed = models.BooleanField(default=False)
     transcribedSpeech = models.TextField(null=True)
+    transcriptionConfidence = models.DecimalField(max_digits=5, decimal_places=5, null=True)
 
-
+class Timestamp(models.Model):
+    audio = models.ForeignKey(Audio)
+    word = models.CharField(max_length =1000)
+    timestampBegin = models.FloatField(null=True)
+    timestampEnd = models.FloatField(null=True)
 
 from . import signals ## should be done with appconfig
