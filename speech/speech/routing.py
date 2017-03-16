658bdf2aa37e54847b822cd7e679ff3f076cf787
rmwdeveloper <rmwdeveloper@gmail.com>
2017-03-16 15:52:59 -0400

    redis

--- speech/speech/routing.py
+++ speech/speech/routing.py
@@ -4,7 +4,7 @@ from .consumers import ws_connect, ws_receive, ws_disconnect
 
 
 channel_routing = [
-    # route('http.request', StaticFilesConsumer()),
+    route('http.request', StaticFilesConsumer()),
     route('websocket.connect', ws_connect),
     route('websocket.receive', ws_receive),
     route('websocket.disconnect', ws_disconnect),
