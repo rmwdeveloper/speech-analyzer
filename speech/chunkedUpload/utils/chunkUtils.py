6e17753df616183cdf9a57983c4287dffdd571d9
rmwdeveloper <rmwdeveloper@gmail.com>
2017-03-18 14:30:25 -0400

    tones

--- speech/chunkedUpload/utils/chunkUtils.py
+++ speech/chunkedUpload/utils/chunkUtils.py
@@ -2,11 +2,17 @@ import os
 from chunkedUpload.models import ChunkedUpload
 from django.conf import settings
 from speech.models import Audio ## todo: move this somewhere. chunkutils should be standalone
-
+from common.globalLogger import GlobalLogger
 
 def concatenateChunks(identifier):
     chunks = ChunkedUpload.objects.filter(resumableIdentifier = identifier).order_by('resumableChunkNumber')
-    location = os.path.join(settings.MEDIA_ROOT, settings.UNTRANSCODED_PREFIX, chunks[0].resumableFilename)
+    try:
+        location = os.path.join(settings.MEDIA_ROOT, settings.UNTRANSCODED_PREFIX, chunks[0].resumableFilename)
+    except IndexError as e:
+        GlobalLogger.error('Tried to get first chunk before concatentation, but it didnt exist.'
+                           'identifier is...%s.  %s') % (identifier, e)
+
+
     for chunk in chunks:
         with open(location, 'w+b') as newFile:
             with open(chunk.file.path, 'rb') as chunkFile:
